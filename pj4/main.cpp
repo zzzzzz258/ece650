58dfe6cfa09a6a0055db0776e62b6a6060b9bb37
zz258 <zz258@duke.edu>
2022-03-07 00:20:20 -0500

    drop and create tables completed

--- pj4/main.cpp
+++ pj4/main.cpp
@@ -6,6 +6,72 @@
 using namespace std;
 using namespace pqxx;
 
+
+/**
+ * drop 4 tables
+ */
+void dropTables(connection &c) {
+  work w(c);
+  w.exec("DROP TABLE IF EXISTS PLAYER;");
+  w.exec("DROP TABLE IF EXISTS TEAM;");
+  w.exec("DROP TABLE IF EXISTS STATE;");
+  w.exec("DROP TABLE IF EXISTS COLOR;");    
+  w.commit();
+  cout << "Old tables dropped" << endl;
+}
+
+/**
+ * create 4 tables
+ */
+void createTables(connection & c) {
+  work W(c);
+  string sql = "CREATE TABLE STATE("\
+    "STATE_ID SERIAL,"              \
+    "NAME VARCHAR(256),"      \
+    "PRIMARY KEY (STATE_ID)"  \
+    ");";
+  W.exec(sql);
+
+  sql = "CREATE TABLE COLOR(" \
+    "COLOR_ID SERIAL,"        \
+    "NAME VARCHAR(256),"      \
+    "PRIMARY KEY (COLOR_ID) " \
+    ");";
+  W.exec(sql);
+
+  sql = "CREATE TABLE TEAM("                                            \
+    "TEAM_ID SERIAL,"                                                   \
+    "NAME VARCHAR(256), "                                               \
+    "STATE_ID INT, "                                                    \
+    "COLOR_ID INT,"                                                     \
+    "WINS INT,"                                                         \
+    "LOSSES INT,"                                                       \
+    "PRIMARY KEY (TEAM_ID),"                                            \
+    "FOREIGN KEY (STATE_ID) REFERENCES STATE(STATE_ID) ON DELETE SET NULL ON UPDATE CASCADE," \
+    "FOREIGN KEY (COLOR_ID) REFERENCES COLOR(COLOR_ID) ON DELETE SET NULL ON UPDATE CASCADE" \
+    ");";
+  W.exec(sql);
+  
+  sql = "CREATE TABLE PLAYER("\
+    "PLAYER_ID SERIAL,"                                                 \
+    "TEAM_ID INT,"                                                      \
+    "UNIFORM_NUM INT,"                                                  \
+    "FIRST_NAME VARCHAR(256),"                                          \
+    "LAST_NAME VARCHAR(256),"                                           \
+    "MPG DOUBLE PRECISION,"                                                          \
+    "PPG DOUBLE PRECISION,"                                                          \
+    "RPG DOUBLE PRECISION,"                                                          \
+    "APG DOUBLE PRECISION,"                                                          \
+    "SPG DOUBLE PRECISION,"                                             \
+    "BPG DOUBLE PRECISION,"                                             \
+    "PRIMARY KEY (PLAYER_ID),"                                          \
+    "FOREIGN KEY (TEAM_ID) REFERENCES TEAM(TEAM_ID) ON DELETE SET NULL ON UPDATE CASCADE" \
+    ");";
+  W.exec(sql);
+  W.commit();
+  cout << "Table created successfully" << endl;    
+}
+
 int main (int argc, char *argv[]) 
 {
 
@@ -27,11 +93,16 @@ int main (int argc, char *argv[])
     return 1;
   }
 
+  //clear all existing data on database ACC_BBALL
+  dropTables(*C);
+  
+  // create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database  
+  createTables(*C);
 
-  //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
-  //      load each table with rows from the provided source txt files
+  //TODO: load each table with rows from the provided source txt files
 
-
+  
+  // exercise select queries
   exercise(C);
 
 
